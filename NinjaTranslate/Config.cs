0b70296efdbb8be1fa3a8094e974f6e6ec9268a0
Patrick Liedtke <Patrickliedtke@web.de>
2015-07-08 14:07:53 +0200

    Config-Klasse hinzugefügt

---
+++ NinjaTranslate/Config.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NinjaTranslate {
+    static class Config {
+        private static String filePath = "config.ini";
+        private static Dictionary<String, String> confValues = new Dictionary<String, String>();
+        private static bool valuesLoaded = false;
+
+        private static bool Load() {
+            if (!File.Exists(Config.filePath))
+                return false;
+
+            String[] lines = File.ReadAllLines(@Config.filePath);
+            foreach (String line in lines) {
+                if (line.StartsWith("//") || line.Trim().Equals(""))
+                    continue;
+                
+                String key = line.Substring(0, line.IndexOf("="));
+                String val = line.Substring(line.IndexOf("=") + 1);
+
+                Config.confValues.Add(key, val);
+            }
+
+            Config.valuesLoaded = true;
+
+            return true;
+        }
+
+        public static String GetValue(String key) {
+            if (!Config.valuesLoaded)
+                if (!Config.Load())
+                    return "";
+
+            if (Config.confValues.ContainsKey(key))
+                return Config.confValues[key];
+            return "";
+        }
+    }
+}
